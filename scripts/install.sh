6051bc2200a1fa03fed16233e52bc6be7a79364c
51r <petar.spasov@hashicorp.com>
2022-05-18 15:32:58 +0300

    edited script

--- scripts/install.sh
+++ scripts/install.sh
@@ -5,5 +5,5 @@ apt-get install -y nginx
 sudo ufw allow 'nginx full'
 ### Add page to nginx
 cd /var/www/html
-wget "https://github.com/51r/html-page/releases/download/1.2.9/release.zip"
+wget "https://github.com/51r/packer-ubuntu-nginx-page/releases/download/1.1.2/release.zip"
 unzip release.zip .
