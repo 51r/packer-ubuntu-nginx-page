0db67460b5277dfc6a22eece8afb2a62c5fc279c
Peter Spasov <petar.spasov@hashicorp.com>
2022-05-18 15:25:09 +0300

    Update install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -3,7 +3,7 @@
 apt-get install -y nginx
 ## allow nginx in firewall
 sudo ufw allow 'nginx full'
-### Add site configuration to nginx
-cd /etc/nginx/sites-available/
+### Add page to nginx
+cd /var/www/html
 wget "https://github.com/51r/html-page/releases/download/1.2.9/release.zip"
 unzip release.zip .
